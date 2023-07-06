200502e23ef2823f48defb542e892b569f55e583
Kwabena Agyeman <kagyeman@ymail.com>
2023-07-06 21:05:00 +0000

    displaying platform icons

---
+++ src/components/PlatformIconList.tsx
@@ -0,0 +1,47 @@
+import { HStack, Icon, Text } from '@chakra-ui/react';
+import {
+  FaWindows,
+  FaPlaystation,
+  FaXbox,
+  FaApple,
+  FaAndroid,
+  FaLinux,
+} from 'react-icons/fa';
+import { MdPhoneIphone } from 'react-icons/md';
+import { SiNintendo } from 'react-icons/si';
+import { BsGlobe } from 'react-icons/bs';
+import { Platform } from '../hooks/useGames';
+
+import { IconType } from 'react-icons/lib/esm/iconBase';
+
+interface Props {
+  platforms: Platform[];
+}
+
+interface IconMap {
+  [key: string]: IconType;
+}
+
+const PlatformIconList = ({ platforms }: Props) => {
+  const iconMap: { [key: string]: IconType } = {
+    pc: FaWindows as IconType,
+    playstation: FaPlaystation as IconType,
+    xbox: FaXbox as IconType,
+    nintendo: SiNintendo as IconType,
+    mac: FaApple as IconType,
+    android: FaAndroid as IconType,
+    linux: FaLinux as IconType,
+    ios: MdPhoneIphone as IconType,
+    web: BsGlobe as IconType,
+  };
+
+  return (
+    <HStack marginY={1}>
+      {platforms.map((platform) => (
+        <Icon as={iconMap[platform.slug]} color={'gray.500'} />
+      ))}
+    </HStack>
+  );
+};
+
+export default PlatformIconList;
