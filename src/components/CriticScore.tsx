af04be4218f7af12c024ada421965b60444025f4
Kwabena Agyeman <kagyeman@ymail.com>
2023-07-06 21:12:27 +0000

    displaying critic scroe

---
+++ src/components/CriticScore.tsx
@@ -0,0 +1,17 @@
+import { Badge } from '@chakra-ui/react';
+
+interface Props {
+  score: number;
+}
+
+const CriticScore = ({ score }: Props) => {
+  const color = score > 75 ? 'green' : score > 60 ? 'yellow' : '';
+
+  return (
+    <Badge colorScheme={color} fontSize={14} paddingX={2} borderRadius={20}>
+      {score}
+    </Badge>
+  );
+};
+
+export default CriticScore;
