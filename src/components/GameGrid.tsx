3d63e446f5cec1f370cfb29e9dcde43b643ba4f9
Kwabena Agyeman <kagyeman@ymail.com>
2023-07-06 22:55:30 +0000

    Added genre styling and fixed responsive design issues

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -16,10 +16,10 @@ const GameGrid = () => {
           sm: 1,
           md: 2,
           lg: 3,
-          xl: 5,
+          xl: 4,
         }}
-        padding={'10'}
-        spacing={10}
+        padding={'10px'}
+        spacing={3}
       >
         {isLoading &&
           skeletons.map((el) => (
