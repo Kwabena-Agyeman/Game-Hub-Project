2bade9015a4ae06c6ba9074ad5d173b9fb78213e
Kwabena Agyeman <kagyeman@ymail.com>
2023-07-08 15:50:06 +0000

    Missing types

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,9 +1,9 @@
-import { Game } from '../hooks/useGames';
 import { Card, CardBody, HStack, Heading, Image } from '@chakra-ui/react';
 import PlatformIconList from './PlatformIconList';
 import CriticScore from './CriticScore';
 import getCroppedImageUrl from '../services/image-url';
 import Emoji from './Emoji';
+import { Game } from '../services/games-service';
 
 interface Props {
   game: Game;
