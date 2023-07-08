9a95443b1b8fa9b5dcef378aecccee9163652dfe
Kwabena Agyeman <kagyeman@ymail.com>
2023-07-08 12:50:02 +0000

    Set up react query

--- src/main.tsx
+++ src/main.tsx
@@ -2,15 +2,33 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { ChakraProvider, ColorModeScript } from '@chakra-ui/react';
 import App from './App.tsx';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import theme from './theme';
 
 import './index.css';
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      // retry: 3,
+      // cacheTime: 300000, //5min
+      // staleTime: 10 * 1000, //10s
+      // refetchOnWindowFocus: false,
+      // refetchOnReconnect: true,
+      // refetchOnMount: true,
+    },
+  },
+});
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ChakraProvider theme={theme}>
-      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
-      <App />
-    </ChakraProvider>
+    <QueryClientProvider client={queryClient}>
+      <ChakraProvider theme={theme}>
+        <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+        <App />
+        <ReactQueryDevtools />
+      </ChakraProvider>
+    </QueryClientProvider>
   </React.StrictMode>
 );
