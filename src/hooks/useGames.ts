03e520ae454269dfe8285dab320845b48a853f62
Kwabena Agyeman <kagyeman@ymail.com>
2023-07-08 13:32:11 +0000

    Removed duplicate interfaces

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,16 +1,6 @@
 import { useQuery } from '@tanstack/react-query';
-import { fetchGames } from '../services/games-service';
+import { Game, fetchGames } from '../services/games-service';
 import { GameQuery } from '../App';
-import { Platform } from '../services/platform-service';
-
-export interface Game {
-  id: number;
-  name: string;
-  background_image: string;
-  parent_platforms: { platform: Platform }[];
-  metacritic: number;
-  rating_top: number;
-}
 
 const useGames = (gameQuery: GameQuery) => {
   return useQuery<Game[], Error>({
