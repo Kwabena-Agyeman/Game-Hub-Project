f64f05782a9658e584e02744fdeb8ecf232e2c94
Kwabena Agyeman <kagyeman@ymail.com>
2023-07-09 21:56:30 +0000

    handle error in navigation

---
+++ src/pages/ErrorPage.tsx
@@ -0,0 +1,24 @@
+import { Box, Heading, Text } from '@chakra-ui/react';
+
+import { useRouteError, isRouteErrorResponse } from 'react-router-dom';
+import NavBar from '../components/NavBar';
+
+const ErrorPage = () => {
+  const error = useRouteError();
+
+  return (
+    <>
+      <NavBar />
+      <Box padding={5}>
+        <Heading>Oops</Heading>
+        <Text>
+          {isRouteErrorResponse(error)
+            ? 'This page does not exists'
+            : 'Something went wrong'}
+        </Text>
+      </Box>
+    </>
+  );
+};
+
+export default ErrorPage;
